80901debc9dfa7a3e17f53d3ee91d9e36adb0095
Zixu Cheng <91916800+Cheng-Zixu@users.noreply.github.com>
2021-11-03 12:07:51 +0000

    Update arduino_secrets.h

--- Plant-Friend/arduino_secrets.h
+++ Plant-Friend/arduino_secrets.h
@@ -1,4 +1,4 @@
 const char* ssid     = "CE-Hub";
-const char* password = "()c454,w1f1";
+const char* password = "password";
 const char* mqttuser = "student";
-const char* mqttpass = "ce2021-mqtt-forget-whale";
+const char* mqttpass = "mqttpassword";
